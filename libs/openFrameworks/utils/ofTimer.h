384f604043fa992668175931531d89da516ad799
Christopher Baker <me@christopherbaker.net>
2016-02-20 00:38:36 -0700

    Documentation updates for ofTimer.h.

--- libs/openFrameworks/utils/ofTimer.h
+++ libs/openFrameworks/utils/ofTimer.h
@@ -6,19 +6,18 @@ public:
 	/// \brief Constructs the default ofTimer().
 	ofTimer();
 
-	/// \brief  Reset the starting instant of the periodic event to now.
+	/// \brief Reset the starting instant of the periodic event to now.
 	void reset();
 	
-	/// \brief Sets the frequency in \p nanoseconds that events happen.
+	/// \brief Sets the timer event interval in nanoseconds.
 	/// 
-	/// The timer will begin calculating the events beginning the moment
-	/// the function is called.
+	/// The timer will begin calculating the event intervals beginning the
+	/// moment the function is called.
+	///
 	/// \param nanoseconds The number of nanoseconds between events.
 	void setPeriodicEvent(uint64_t nanoseconds);
 	
-	/// \brief Sleep this thread until the next periodic event.
-	/// 
-	/// \warning This will crash your application if you haven't called setPeriodicEvent().
+	/// \brief Sleep the current thread until the next periodic event.
 	void waitNext();
 private:
 	void calculateNextPeriod();
